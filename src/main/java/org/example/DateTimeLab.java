eec26b7d0c44314f71e8a6e9fac6f1f3901f2b9d
Godfrey Angelo Convento <godfrey-a.convento@gcash.com>
2025-08-07 17:18:19 +0800

    feat: complete exercise 3

--- src/main/java/org/example/DateTimeLab.java
+++ src/main/java/org/example/DateTimeLab.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
@@ -10,19 +11,27 @@ public class DateTimeLab {
         // LocalDate today = LocalDate.of(2025, 8, 21);
 
         // DateTimeFormatter dtf1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        // DateTimeFormatter dtf2 = DateTimeFormatter.ofPattern("EEEE, MMMM dd, yyyy", Locale.ENGLISH);
+        // DateTimeFormatter dtf2 = DateTimeFormatter.ofPattern("EEEE, MMMM dd, yyyy",
+        // Locale.ENGLISH);
 
         // System.out.println("Default format: " + today);
         // System.out.println("Custom format 1: " + today.format(dtf1));
         // System.out.println("Custom format 2: " + today.format(dtf2));
 
-        LocalTime now = LocalTime.of(16, 45, 30);
+        // LocalTime now = LocalTime.of(16, 45, 30);
 
-        DateTimeFormatter dtf1 = DateTimeFormatter.ofPattern("HH:mm:ss");
-        DateTimeFormatter dtf2 = DateTimeFormatter.ofPattern("hh:mm:ss a");
+        // DateTimeFormatter dtf1 = DateTimeFormatter.ofPattern("HH:mm:ss");
+        // DateTimeFormatter dtf2 = DateTimeFormatter.ofPattern("hh:mm:ss a");
 
-        System.out.println("Default format: " + now);
-        System.out.println("24-hour format: " + now.format(dtf1));
-        System.out.println("12-hour format with AM/PM: " + now.format(dtf2));
+        // System.out.println("Default format: " + now);
+        // System.out.println("24-hour format: " + now.format(dtf1));
+        // System.out.println("12-hour format with AM/PM: " + now.format(dtf2));
+
+        LocalDateTime event = LocalDateTime.of(2025, 11, 27, 19, 0, 0);
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MMM dd, yyyy 'at' hh:mm a");
+
+        System.out.println("Default format: " + event);
+        System.out.println("Custom format: " + event.format(dtf));
     }
 }
